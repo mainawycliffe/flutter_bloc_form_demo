d90c474d27d16c9b1a33c13f2d1d553c7bb3727f
Maina Wycliffe <wmmaina7@gmail.com>
2021-01-06 03:25:44 +0300

    Add Events For For Managing Contact Form State

--- lib/bloc/contact_form_event.dart
+++ lib/bloc/contact_form_event.dart
@@ -6,3 +6,52 @@ abstract class ContactFormEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class NameChangedEvent extends ContactFormEvent {
+  final String name;
+
+  const NameChangedEvent(this.name);
+
+  @override
+  List<Object> get props => [name];
+}
+
+class EmailChangedEvent extends ContactFormEvent {
+  final String email;
+
+  const EmailChangedEvent(this.email);
+
+  @override
+  List<Object> get props => [email];
+}
+
+class PhoneChangedEvent extends ContactFormEvent {
+  final String phone;
+
+  const PhoneChangedEvent(this.phone);
+
+  @override
+  List<Object> get props => [phone];
+}
+
+class SubjectChangedEvent extends ContactFormEvent {
+  final String subject;
+
+  const SubjectChangedEvent(this.subject);
+
+  @override
+  List<Object> get props => [subject];
+}
+
+class MessageChangedEvent extends ContactFormEvent {
+  final String message;
+
+  const MessageChangedEvent(this.message);
+
+  @override
+  List<Object> get props => [message];
+}
+
+class ContactFormSubmittedEvent extends ContactFormEvent {
+  const ContactFormSubmittedEvent();
+}
